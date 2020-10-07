623cd238f3df1eaf1a11d3f6dee8b3d3479018f5
Kenneth Schiller <Kenadia@users.noreply.github.com>
2020-10-07 14:32:58 -0700

    export keys

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { MARGIN_TOKEN } from './constants';
 export * from './helpers';
+export * from './keys';
 export { default as Order } from './order';
 export * from './types';
 export { default as Withdrawal } from './withdrawal';
