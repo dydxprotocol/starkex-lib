c336d239bd5cbc715c12f6c4c662ea3ae6a16993
samweinberg23 <samweinberg23@gmail.com>
2020-10-20 11:33:57 -0700

    export api request (#14)

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export { MARGIN_TOKEN } from './constants';
 export * from './helpers';
 export * from './keys';
 export { default as Order } from './order';
+export { default as ApiRequest } from './api-request';
 export * from './types';
 export { default as Withdrawal } from './withdrawal';
