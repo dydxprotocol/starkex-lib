adde8a95b09047e518b1bd162342254f88a64c50
Sam Weinberg <samweinberg23@gmail.com>
2020-11-06 11:29:51 -0800

    expose registration

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from './helpers';
 export * from './keys';
 export { default as Order } from './order';
 export { default as ApiRequest } from './api-request';
+export { default as Registration } from './registrations';
 export * from './types';
 export { default as Withdrawal } from './withdrawal';
