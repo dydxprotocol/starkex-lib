fdcdb471a4d40e8f6690ce292396f9e30266f642
Kenadia <Kenadia@users.noreply.github.com>
2021-01-06 13:23:06 -0800

    Add inline licenses for Starkware code (#24)

--- src/lib/starkex-resources/constant-points.ts
+++ src/lib/starkex-resources/constant-points.ts
@@ -1,3 +1,19 @@
+/////////////////////////////////////////////////////////////////////////////////
+// Copyright 2019 StarkWare Industries Ltd.                                    //
+//                                                                             //
+// Licensed under the Apache License, Version 2.0 (the "License").             //
+// You may not use this file except in compliance with the License.            //
+// You may obtain a copy of the License at                                     //
+//                                                                             //
+// https://www.starkware.co/open-source-license/                               //
+//                                                                             //
+// Unless required by applicable law or agreed to in writing,                  //
+// software distributed under the License is distributed on an "AS IS" BASIS,  //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    //
+// See the License for the specific language governing permissions             //
+// and limitations under the License.                                          //
+/////////////////////////////////////////////////////////////////////////////////
+
 export const constantPointsHex = [
   [
     '49ee3eba8c1600700ee1b87eb599f16716b0b1022947733551fde4050ca6804',
