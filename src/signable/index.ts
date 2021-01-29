9b2062b5a5f25033e8a46cefc0b8a94c24634659
Kenneth Schiller <Kenadia@users.noreply.github.com>
2021-01-29 11:34:01 -0800

    Add exports

--- src/signable/index.ts
+++ src/signable/index.ts
@@ -1,5 +1,9 @@
 export { SignableApiRequest } from './api-request';
-export { preComputeHashes } from './hashes';
+export {
+  preComputeHashes,
+  setGlobalStarkHashImplementation,
+  setGlobalStarkHashImplementationNoSanityCheck,
+} from './hashes';
 export { SignableOraclePrice } from './oracle-price';
 export { SignableOrder } from './order';
 export { SignableWithdrawal } from './withdrawal';
