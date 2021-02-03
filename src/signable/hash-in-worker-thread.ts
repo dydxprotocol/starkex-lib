73932ca6bda57cfce3fd3dca77cfcb97a9e7b506
Kenneth Schiller <Kenadia@users.noreply.github.com>
2021-02-02 17:33:51 -0800

    Require worker_threads to be injected

--- src/signable/hash-in-worker-thread.ts
+++ src/signable/hash-in-worker-thread.ts
@@ -1,47 +1,54 @@
-import {
-  Worker,
-  isMainThread,
-  parentPort,
-  workerData,
-} from 'worker_threads';
-
 import BN from 'bn.js';
 
 import { pedersen } from '../lib/starkex-resources';
 import { HashFunction } from '../types';
 
-let hashFunction: HashFunction = function hashInWorkerThread(_a: BN, _b: BN) {
-  throw new Error('Expected hashInWorkerThread() to be called from the main thread');
-};
+/**
+ * Make the hashInWorkerThread function by passing in the Node worker_threads function.
+ */
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+export function hashInWorkerThread(workerThreadsModule: any) {
+  const {
+    Worker,
+    isMainThread,
+    parentPort,
+    workerData,
+  } = workerThreadsModule;
 
-if (isMainThread) {
-  /**
-   * Pedersen hash implementation that runs in a worker thread.
-   */
-  hashFunction = function hashInWorkerThread(a: BN, b: BN): Promise<BN> {
-    return new Promise((resolve, reject) => {
-      const worker = new Worker(
-        __filename, {
-          workerData: {
-            a: a.toString(),
-            b: b.toString(),
-          },
-        },
-      );
-      worker.on('message', (hashResult) => {
-        resolve(new BN(hashResult));
-      });
-      worker.on('error', reject);
-      worker.on('exit', (code) => {
-        if (code !== 0) {
-          reject(new Error(`Worker stopped with exit code ${code}`));
-        }
-      });
-    });
+  let hashFunction: HashFunction = function hashInWorkerThreadInner(_a: BN, _b: BN) {
+    throw new Error('Expected hashInWorkerThread() to be called from the main thread');
   };
-} else {
-  const { a, b }: { a: string, b: string } = workerData;
-  const hashResult = pedersen(new BN(a), new BN(b)).toString();
-  parentPort!.postMessage(hashResult);
+
+  if (isMainThread) {
+    /**
+     * Pedersen hash implementation that runs in a worker thread.
+     */
+    hashFunction = function hashInWorkerThreadInner(a: BN, b: BN): Promise<BN> {
+      return new Promise((resolve, reject) => {
+        const worker = new Worker(
+          __filename, {
+            workerData: {
+              a: a.toString(),
+              b: b.toString(),
+            },
+          },
+        );
+        worker.on('message', (hashResult: string) => {
+          resolve(new BN(hashResult));
+        });
+        worker.on('error', reject);
+        worker.on('exit', (code: number) => {
+          if (code !== 0) {
+            reject(new Error(`Worker stopped with exit code ${code}`));
+          }
+        });
+      });
+    };
+  } else {
+    const { a, b }: { a: string, b: string } = workerData;
+    const hashResult = pedersen(new BN(a), new BN(b)).toString();
+    parentPort!.postMessage(hashResult);
+  }
+
+  return hashFunction;
 }
-export const hashInWorkerThread = hashFunction;
