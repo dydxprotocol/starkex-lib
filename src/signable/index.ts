ecd353826548f17b8a7157379081d5ebd95a7037
Kenneth Schiller <Kenadia@users.noreply.github.com>
2021-02-02 17:06:02 -0800

    Make worker_thread import optional

--- src/signable/index.ts
+++ src/signable/index.ts
@@ -1,5 +1,8 @@
+import BN from 'bn.js';
+
+import { HashFunction } from '../types';
+
 export { SignableApiRequest } from './api-request';
-export { hashInWorkerThread } from './hash-in-worker-thread';
 export {
   preComputeHashes,
   setGlobalStarkHashImplementation,
@@ -9,3 +12,18 @@ export { SignableOraclePrice } from './oracle-price';
 export { SignableOrder } from './order';
 export { SignableWithdrawal } from './withdrawal';
 export { SignableConditionalTransfer } from './conditional-transfer';
+
+let maybeHashInWorkerThread: HashFunction = (_a: BN, _b: BN) => {
+  throw new Error('Cannot use hashInWorkerThread() since worker_threads is not available');
+};
+try {
+  /* eslint-disable @typescript-eslint/no-var-requires,global-require */
+  require('worker_threads');
+  // If the worker_threads module is available, update maybeHashInWorkerThread.
+  maybeHashInWorkerThread = require('./hash-in-worker-thread').hashInWorkerThread;
+  /* eslint-enable @typescript-eslint/no-var-requires,global-require */
+} catch (error) {
+  // eslint: Intentionally empty.
+}
+
+export const hashInWorkerThread = maybeHashInWorkerThread;
